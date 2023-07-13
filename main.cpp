6d6ae85bdffd2af12d5c0f66f58e3f34d743b1a5
Hasan775 <gasanveliev3@gmail.com>
2023-07-13 21:55:31 +0400

    holding added

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #include "Block.h"
 using namespace std;
 using namespace std::chrono;
-int DrawFrame(Zone zone, Shape shape){
+int DrawFrame(Zone zone, Shape shape, char holding){
     vector<vector<char>> v;
     system("cls");
     for (int i = 0; i < WIDTH; i++){
@@ -43,14 +43,16 @@ int DrawFrame(Zone zone, Shape shape){
     for (int i = 0; i < WIDTH; i++){
        cout<<"*";
     }
+    cout<<"\n\n\nHolded: "<<holding;
     return 0;
 }
 int main(){
     Zone zone = Zone();
     Shape shape = Shape();
-    vector<char> blocks = {'J', 'L', 'O', 'T', 'S', 'Z', 'I'};  
+    vector<char> blocks = {'J', 'L', 'O', 'T', 'S', 'Z', 'I'};
+    char holded = ' ';
     int code = 0, block = 0;
-    bool isShapeSpawned = false, isRotationPressed = false;
+    bool isShapeSpawned = false, isRotationPressed = false, isHoldingUsed;
     unsigned long diff = 0, fdiff = 0;
     auto lastClock = high_resolution_clock::now().time_since_epoch().count();
     auto newClock = high_resolution_clock::now().time_since_epoch().count();
@@ -61,6 +63,7 @@ int main(){
         fdiff += newClock - lastClock;
         if (!isShapeSpawned){
             isShapeSpawned = true;
+            isHoldingUsed = false;
             srand(time(0));
             block = rand() % blocks.size();
             zone.Check();
@@ -71,10 +74,10 @@ int main(){
                 return 0;
             }
         }
-        if (diff >= CHRONOSECOND)
+        if (diff >= 0.5 * CHRONOSECOND)
         {
             isShapeSpawned = shape.Fall(&zone.v);
-            diff -= CHRONOSECOND;
+            diff -= 0.5 * CHRONOSECOND;
         }
         if (kbhit()){
             code = getch();
@@ -87,11 +90,26 @@ int main(){
             if (code == 100){
                 shape.Right(zone.v);
             }
-            DrawFrame(zone, shape);
+            if (code == 99 && !isHoldingUsed){
+                isHoldingUsed = true;
+                if (holded == ' '){
+                    holded = shape.type;
+                    srand(time(0));
+                    block = rand() % blocks.size();
+                    zone.Check();
+                    shape.Spawn(blocks[block], zone.v);
+                }
+                else{
+                    char nh = holded;
+                    holded = shape.type;
+                    shape.Spawn(nh, zone.v);
+                }
+            }
+            DrawFrame(zone, shape, holded);
         }
         if (fdiff >= 0.5 * CHRONOSECOND)
         {
-            DrawFrame(zone, shape);
+            DrawFrame(zone, shape, holded);
             fdiff -= 0.5 * CHRONOSECOND;
         }
     }
